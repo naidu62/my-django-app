3c0b2daf9b1de4339ff47c3130b1d09308911e8d
naidu62 <vn04081994@gmail.com>
2025-04-12 03:19:25 +0530

    Update settings.py

--- jobcat_project/settings.py
+++ jobcat_project/settings.py
@@ -47,12 +47,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jobcat_project.wsgi.application'
 
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        conn_max_age=600
+    )
 }
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
