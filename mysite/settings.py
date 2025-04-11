dba305d07d5c33183baa29f99d27241e541285d3
naidu62 <vn04081994@gmail.com>
2025-04-11 14:02:26 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,18 +9,44 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# ✅ Admin-related apps
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
+# ✅ Needed for sessions, messages, security, etc.
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
 
-TEMPLATES = []
+# ✅ Template engine for rendering admin/login pages
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
